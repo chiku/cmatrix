206d8a93c6f4ec26d9c921d52e90e11c74a184d6
Chiku <chirantan.mitra@gmail.com>
2011-06-21 23:32:00 +0530

    Exceptions should inherit from proper exception classes

--- src/matrix_exceptions.h
+++ src/matrix_exceptions.h
@@ -3,18 +3,42 @@
 #ifndef MATRIX_EXCEPTIONS_H
 #define MATRIX_EXCEPTIONS_H
 
+#include <stdexcept>
+
 namespace CMatrix
 {
     class MatrixException
     {
         public:
-            class InvalidSizeSet {};
-            class SizeResetException {};
-            class AccessOutOfBound {};
+            class InvalidSizeSet : public std::runtime_error
+            {
+                public: InvalidSizeSet() : std::runtime_error("Invalid size set") {}
+            };
 
-            class IncompatibleAddition {};
-            class IncompatibleSubtraction {};
-            class IncompatibleMultiplication {};
+            class SizeResetException : public std::runtime_error
+            {
+                public: SizeResetException() : std::runtime_error("Invalid size set") {}
+            };
+
+            class AccessOutOfBound : public std::runtime_error
+            {
+                public: AccessOutOfBound() : std::runtime_error("Attempt to access out of bound") {}
+            };
+
+            class IncompatibleAddition : public std::runtime_error
+            {
+                public: IncompatibleAddition() : std::runtime_error("Incompatible addition") {}
+            };
+
+            class IncompatibleSubtraction : public std::runtime_error
+            {
+                public: IncompatibleSubtraction() : std::runtime_error("Incompatible subtraction") {}
+            };
+
+            class IncompatibleMultiplication : public std::runtime_error
+            {
+                public: IncompatibleMultiplication() : std::runtime_error("Incompatible multiplication") {}
+            };
     };
 }
 
