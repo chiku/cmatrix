2dec322b4a3b5b84e03cbaa6f47552b98eb08da5
Chiku <chirantan.mitra@gmail.com>
2011-06-25 08:32:46 +0530

    Delete duplicate function

--- src/matrix_io_png.cpp
+++ src/matrix_io_png.cpp
@@ -3,20 +3,6 @@
 #include <matrix.h>
 #include <vendor/pngwriter/pngwriter.h>
 
-// Output to console
-template <class Type>
-std::ostream& CMatrix::operator << (std::ostream& outputStream, const CMatrix::Matrix<Type>& firstMatrix)
-{
-    for(int i = 0; i < firstMatrix.rows(); i++)
-    {
-        for(int j = 0; j< firstMatrix.columns(); j++)
-            outputStream << firstMatrix(i, j) << '\t';
-        outputStream << '\n';
-    }
-
-    return outputStream;
-}
-
 // Output a png using constant colour scale applied for all colors
 template <class Type>
 void CMatrix::Matrix<Type>::pngize(const char* fileName, Type redScale, Type blueScale, Type greenScale) const
