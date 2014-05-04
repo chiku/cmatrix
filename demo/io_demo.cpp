903cbd2ed389f9ff8b4cfe6725384dc7a6aaa4da
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-05-04 20:24:03 +0530

    Don't use "using namespace" in demo

--- demo/io_demo.cpp
+++ demo/io_demo.cpp
@@ -1,24 +1,22 @@
 // Written by     : Chirantan Mitra
 
 #include <cmath>
-#include <matrix.h>
+
+#include "matrix.h"
 
 class testFunctionoid
 {
-    public:
-        double operator() (double x, double y)
-        {
-            return (sin(x * y) + 1.0) / 2.0;
-        }
+public:
+    double operator() (double x, double y)
+    {
+        return (sin(x * y) + 1.0) / 2.0;
+	}
 };
-
-
-using namespace cmatrix;
 
 int main()
 {
     int size = 5;
-    Matrix<double> matrix(size, size);
+    cmatrix::Matrix<double> matrix(size, size);
     matrix.fillByPosition(new testFunctionoid, size/2, size/2, 1.0/size, 1.0/size);
     std::cout << matrix << std::endl;
 }
